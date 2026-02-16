3281d4ca35a921a11d629638f8ebd946800a6272
Nurali Zharylgassyn <65157245+zharylgassyn@users.noreply.github.com>
2026-02-16 22:27:32 +0500

    Create chapter2_11.js

---
+++ lab3/task1/chapter2_11.js
@@ -0,0 +1,51 @@
+alert( null || 2 || undefined );
+
+alert( alert(1) || 2 || alert(3) );
+
+alert(1 && null && 2);
+
+alert( alert(1) && alert(2) );
+
+alert( null || 2 && 3 || 4 );
+
+if (age >= 14 && age <= 90)
+
+if (!(age >= 14 && age <= 90))
+
+if (age < 14 || age > 90)
+
+// Runs.
+// The result of -1 || 0 = -1, truthy
+if (-1 || 0) alert( 'first' );
+
+// Doesn't run
+// -1 && 0 = 0, falsy
+if (-1 && 0) alert( 'second' );
+
+// Executes
+// Operator && has a higher precedence than ||
+// so -1 && 1 executes first, giving us the chain:
+// null || -1 && 1  ->  null || 1  ->  1
+if (null || -1 && 1) alert( 'third' );
+
+
+
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'Welcome!' );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
