6b671aba71f4097f836ff94ce12f8917a8b835cd
Nurali Zharylgassyn <65157245+zharylgassyn@users.noreply.github.com>
2026-02-25 04:25:51 +0500

    lab 4

---
+++ lab4/task1/13.ts
@@ -0,0 +1,11 @@
+import {Routes} from '@angular/router';
+import {Home} from './home/home';
+import {User} from './user/user';
+
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'App Home Page',
+    component: Home,
+  },
+];
