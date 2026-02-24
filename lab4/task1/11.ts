441dc7a3ba6fcd1e385c50cd9a9348f96f26f175
Nurali Zharylgassyn <65157245+zharylgassyn@users.noreply.github.com>
2026-02-25 03:49:32 +0500

    l

---
+++ lab4/task1/11.ts
@@ -0,0 +1,26 @@
+import {Component} from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>Preferred Framework:</p>
+    <ul>
+      <li>
+        Static Image:
+        <img ngSrc="/logo.svg" alt="Angular logo" />
+      </li>
+      <li>
+        Dynamic Image:
+        <img [ngSrc]="logoUrl" [alt]="logoAlt" />
+      </li>
+    </ul>
+  `,
+  imports: [NgOptimizedImage],
+})
+export class User {
+  logoUrl = '/logo.svg';
+  logoAlt = 'Angular logo';
+  username = 'youngTech';
+}
