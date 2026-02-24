9699be6906a55d25a1dd5c0073ad5c90e80cda2a
Nurali Zharylgassyn <65157245+zharylgassyn@users.noreply.github.com>
2026-02-25 04:37:36 +0500

    lab 4

---
+++ lab4/task1/15.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-user',
+  template: `
+    <p>Username: {{ username }}</p>
+    <p>{{ username }}'s favorite framework: {{ favoriteFramework }}</p>
+    <label for="framework">Favorite Framework: <input id="framework" type="text" [(ngModel)]="favoriteFramework" /></label>
+  `,
+  imports: [FormsModule],
+})
+export class User {
+  username = 'youngTech';
+  favoriteFramework = '';
+}
