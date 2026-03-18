187c2b38568d20ad76fd2106f90bc48c9d118a3b
Nurali Zharylgassyn <65157245+zharylgassyn@users.noreply.github.com>
2026-03-18 09:20:34 +0500

    lab 7

---
+++ lab7/task1/informatics/conditional/c.py
@@ -0,0 +1,6 @@
+a = int(input())
+b = int(input())
+if (a == b and a == 1) or (a != 1 and b!= 1):
+    print("Yes")
+else:
+    print("No")
---
+++ lab7/task1/informatics/conditional/e.py
@@ -0,0 +1,8 @@
+a = int(input())
+b = int(input())
+if b < a:
+    print(a)
+elif b > a:
+    print(b)
+else:
+    print(0)
